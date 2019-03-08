a2b9e9cc3047e9f44c023dae8d0ad4b96e7830ad
Haitao <haitaoprunus@gmail.com>
2019-03-08 16:02:04 +0000

    correct build script

--- Prometheus_Metric_Publish_Pod/build_image.sh
+++ Prometheus_Metric_Publish_Pod/build_image.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export dockerUser=
+export dockerPwd=
 
 env GOOS=linux GOARCH=amd64 go build
 
-docker login -u khitaomei -p galaxy123
+docker login -u $dockerUser -p $dockerPwd
 
 docker build -t khitaomei/custommetric:latest .
 docker push khitaomei/custommetric:latest
